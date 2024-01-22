4a912d76609d52caa2073a456ba4a2ffca1a39ba
boyeon <qhdus96@naver.com>
2024-01-23 01:16:18 +0900

    Feat: blog-category
    - category 디자인 수정
    - hover, click 디자인 추가
    - post list 디자인 수정(카테고리 링크 추가)

--- src/components/category_list.tsx
+++ src/components/category_list.tsx
@@ -2,6 +2,8 @@ import { FC } from "react"
 import StyledTypography from "./atoms/styled_typography"
 import Button from "./foundations/button"
 import { Link } from "gatsby"
+import FlexBox from "./atoms/flex_box"
+import colors from "../constants/colors"
 
 export type CategoryListProps = {
     selectedCategory: string
@@ -15,17 +17,32 @@ const CategoryList: FC<CategoryListProps> = ({
     categoryList,
 }: CategoryListProps) => {
     return (
-        <div>
-            {
-                Object.entries(categoryList).map(([name, count]) => (
-                    <Link to={`./?category=${name}`} key={name}>
-                        <StyledTypography color="gray2" css={{ marginRight: '10px' }} >
-                            #{name}({count})
-                        </StyledTypography>
-                    </Link>
-                ))
-            }
-        </div>
+        <FlexBox gap="50px" align="center">
+            <FlexBox gap="10px" align="center">
+                <StyledTypography variant="h1B">{selectedCategory}</StyledTypography>
+                <StyledTypography variant="h4" color="gray2">{categoryList[selectedCategory]} POSTS</StyledTypography>
+            </FlexBox>
+            <FlexBox direction="row" gap="10px" css={{ flexWrap: 'wrap' }} justify="center">
+                {
+                    Object.entries(categoryList).map(([name]) => (
+                        <Link to={`./?category=${name}`} key={name}>
+                            <StyledTypography
+                                variant={name === selectedCategory ? "h6B" : "h6"}
+                                color={name === selectedCategory ? "primary3" : "gray2"}
+                                css={{
+                                    marginRight: '10px',
+                                    '&:hover': {
+                                        color: colors.primary3
+                                    }
+                                }} >
+                                {name}
+                            </StyledTypography>
+                        </Link>
+                    ))
+                }
+            </FlexBox>
+
+        </FlexBox >
     )
 }
 
--- src/components/post_list.tsx
+++ src/components/post_list.tsx
@@ -2,9 +2,14 @@ import { Link } from "gatsby-link";
 import colors from "../constants/colors";
 import FlexBox from "./atoms/flex_box";
 import StyledTypography from "./atoms/styled_typography";
-import Icon from "./atoms/icon";
 import { Post } from "../models/types";
 import useInfiniteScroll from "../hooks/use-infinite-scroll";
+import { css } from "@emotion/react";
+
+const articleStyles = {
+    article: css({ padding: "30px 0" }),
+    chip: css({ backgroundColor: colors.gray12, borderRadius: '8px', padding: '2px 7px' })
+}
 
 const PostList = ({ posts, selectedCategory }: { posts: Post[], selectedCategory: string }) => {
     const { containerRef, slicedPosts } = useInfiniteScroll(selectedCategory, posts);
@@ -13,35 +18,49 @@ const PostList = ({ posts, selectedCategory }: { posts: Post[], selectedCategory
         <section>
             <div ref={containerRef}>
                 {slicedPosts.map((post, index) => (
-                    <Article key={index} {...post} />
+                    <Article key={index} {...post} selectedCategory={selectedCategory} />
                 ))}
             </div>
         </section>)
 }
 
-const Article = ({ node: { frontmatter, fields: { slug } } }: Post) => {
+
+const Article = ({ selectedCategory, node: { frontmatter, fields: { slug } } }: Post & { selectedCategory: string }) => {
     return (
-        <article
-            css={{
-                // borderBottom: `2px solid ${colors.gray9}`,
-                padding: "30px 20px",
-            }}
-        >
+        <article css={articleStyles.article}>
             <Link to={slug} >
-                <FlexBox css={{ gap: "20px" }}>
+                <FlexBox gap="20px" >
                     <StyledTypography variant="h3">{frontmatter.title}</StyledTypography>
                     <StyledTypography color="gray2" variant="h6">
                         {frontmatter.summary}
                     </StyledTypography>
-                    <FlexBox direction="row" css={{ gap: "5px" }}>
-                        {/* <Icon size="1x" name="calendar" /> */}
-                        <StyledTypography color="gray2">
+                    <FlexBox direction="row" gap="5px" css={{ width: '100%' }} justify="space-between" align="center">
+                        <StyledTypography color="gray5">
                             {frontmatter.date}
                         </StyledTypography>
+                        <CategoryChips categories={frontmatter.categories} selectedCategory={selectedCategory} />
                     </FlexBox>
                 </FlexBox>
             </Link>
         </article >
+    )
+}
+
+const CategoryChips = ({ categories, selectedCategory }: { categories: string[], selectedCategory: string }) => {
+    return (
+        <FlexBox direction="row" gap="5px">
+            {categories.map((category: string) => (
+                <Link to={`./?category=${category}`} key={category}>
+                    <div css={articleStyles.chip}>
+                        <StyledTypography
+                            color={selectedCategory === category ? "primary3" : "gray5"}
+                            variant={selectedCategory === category ? "h7B" : "h7"}>
+                            {category}
+                        </StyledTypography>
+                    </div>
+                </Link>
+            ))}
+        </FlexBox>
     )
 }
 
